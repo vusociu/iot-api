a953f14a01f2df85d191da36461e2224996c2567
vusociu <vu13102003@gmail.com>
2025-11-08 15:44:26 +0700

    cònig

--- iot-project/Program.cs
+++ iot-project/Program.cs
@@ -25,7 +25,16 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IIdentityCardRepository, IdentityCardRepository>();
 builder.Services.AddScoped<ICheckCardHistoryRepository, CheckCardHistoryRepository>();
 builder.Services.AddScoped<JwtService>();
-builder.Services.AddSingleton<MqttService>(sp =>new MqttService("192.168.1.13", 1883, "vund", "131003"));
+builder.Services.AddSingleton<MqttService>(sp =>
+{
+    var mqttConfig = builder.Configuration.GetSection("Mqtt");
+    return new MqttService(
+        mqttConfig["Server"] ?? throw new InvalidOperationException("Mqtt:Server is not configured"),
+        int.Parse(mqttConfig["Port"] ?? throw new InvalidOperationException("Mqtt:Port is not configured")),
+        mqttConfig["Username"] ?? throw new InvalidOperationException("Mqtt:Username is not configured"),
+        mqttConfig["Password"] ?? throw new InvalidOperationException("Mqtt:Password is not configured")
+    );
+});
 builder.Services.AddHostedService<MqttSubcriberService>();
 builder.Services.AddMemoryCache();
 var app = builder.Build();
